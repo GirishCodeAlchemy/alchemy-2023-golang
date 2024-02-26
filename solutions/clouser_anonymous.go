1740cd9e00667e4e161caf276aff4eb070da639a
GirishCodeAlchemy <venkatareddygirish@gmail.com>
2024-02-26 00:19:07 -0500

    Added closure anonymous code

---
+++ solutions/clouser_anonymous.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+// This function intSeq returns another function, which we define anonymously
+// in the body of intSeq. The returned function closes over the variable i to form a closure.
+
+// We call intSeq, assigning the result (a function) to nextInt.
+//  This function value captures its own i value, which will be updated each time we call nextInt.
+
+func intseq() func() int {
+
+	i := 0
+	return func() int {
+		i++
+		return i
+
+	}
+
+}
+
+func main() {
+
+	newseq := intseq()
+	fmt.Println(newseq())
+	fmt.Println(newseq())
+	fmt.Println(newseq())
+	fmt.Println(newseq())
+	newseq = intseq()
+	fmt.Println(newseq())
+	fmt.Println(newseq())
+
+}
