78a4802184b9fe1dd18bc729687194a07b23df1e
GirishCodeAlchemy <venkatareddygirish@gmail.com>
2024-02-27 14:36:59 -0500

    Added the parallel character fetch

---
+++ solutions/parallel_characters_split_mutex_deepfence.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"sync"
+	"time"
+)
+
+var totalcount int = 0
+var failurecount int = 0
+
+func worker(finaldata int, wg *sync.WaitGroup, m *sync.Mutex) {
+	defer wg.Done()
+	status := checkWithRemoteService(finaldata)
+	fmt.Println(status, finaldata)
+	m.Lock()
+	if status {
+		totalcount++
+	} else {
+		failurecount++
+	}
+	m.Unlock()
+}
+
+func main() {
+
+	// TODO
+	var wg sync.WaitGroup
+	var m sync.Mutex
+	tmpString := ""
+
+	for {
+
+		b := getNextChar()
+		if b == 0 {
+			break
+		} else {
+			tmp := string(b)
+			if tmp != ";" {
+				_, ok := strconv.Atoi(tmp)
+				if ok == nil {
+					tmpString += tmp
+				}
+			} else {
+				finaldata, err := strconv.Atoi(tmpString)
+				if err != nil {
+					print(err)
+				}
+				fmt.Println(finaldata)
+				wg.Add(1)
+				go worker(finaldata, &wg, &m)
+				tmpString = ""
+			}
+
+		}
+	}
+	wg.Wait()
+	fmt.Println(totalcount, failurecount)
+
+}
+
+const (
+	stream = "12afsd3;345;678;901;234;578"
+)
+
+// SKIP BELOW
+
+var (
+	index = 0
+)
+
+func getNextChar() byte {
+
+	if index < len(stream) {
+
+		tmp := stream[index]
+
+		index += 1
+
+		return tmp
+
+	}
+
+	return 0
+
+}
+
+func checkWithRemoteService(cve_signature int) bool {
+
+	time.Sleep(5 * time.Second)
+
+	rand.Seed(time.Now().UnixNano())
+
+	return rand.Intn(2) == 1
+
+}
